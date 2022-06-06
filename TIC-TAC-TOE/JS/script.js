7e331b3b76cb96638a9b2e62dc4db28298e5a3c5
Arya Sena Aji <aryasa1997@gmail.com>
2022-06-06 22:07:35 +0700

    Beautify and add logic

--- TIC-TAC-TOE/JS/script.js
+++ TIC-TAC-TOE/JS/script.js
@@ -3,30 +3,38 @@ const circle = "circle";
 
 const boardPlace = document.querySelectorAll(".box");
 const resetButton = document.getElementById("reset");
+const winnerBox = document.getElementById("winner");
+const winningCond = [
+  [0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]
+]
 
 let turn = cross;
 
-function placeMark(target,classAdd){
-  if (turn == cross){
-    target.innerHTML = "X";
-    target.classList.add(classAdd);
-    target.disabled=true;
-    return turn = circle;
+function placeMark(target){
+    target.classList.add(turn);
+    
+    if (turn==cross){
+      target.innerHTML = "X";
+    }
+    else{
+      target.innerHTML = "O";
+    }
+    if (checkWin(turn)){
+      winnerBox.innerHTML = `${turn} wins the game.`;
+      boardPlace.forEach((box) =>
+        box.removeEventListener("click",klik,{once:true})
+        )}
+    else if (isDraw()){
+      winnerBox.innerHTML = "This game ends in draw.";
+    }
+    turn==cross ? turn = circle: turn = cross;
   }
-  else {
-    target.innerHTML = "O";
-    target.classList.add(classAdd);
-    target.disabled=true;
-    return turn = cross;
-  }
-}
 
 function klik (opt) {
   const targeted = opt.target;
   placeMark(targeted,turn);
   }
   
-
 function chooseBox () {
   boardPlace.forEach((box) => {
     box.addEventListener("click",klik,{once : true});
@@ -36,17 +44,31 @@ function chooseBox () {
 
 function reset() {
     boardPlace.forEach((box) => {
-        box.innerHTML="";
-        box.classList.remove("cross");
-        box.classList.remove("circle");
-        turn=cross;
-        chooseBox();
+      box.innerHTML="";
+      box.classList.remove("cross");
+      box.classList.remove("circle");
+      winnerBox.innerHTML = "";
+      turn=cross;
+      chooseBox();
     });
+}
+
+function checkWin(crosAndCirc) {
+  return winningCond.some(combination => {
+    return combination.every((index) => {
+    return boardPlace[index].classList.contains(crosAndCirc)
+  })
+})
+}
+
+function isDraw () {
+  return [...boardPlace].every(box => {
+    return box.classList.contains(cross) || box.classList.contains(circle)
+  })
 }
 
 function resetBox() {
     resetButton.addEventListener("click",reset)
 }
-
 
 chooseBox();
